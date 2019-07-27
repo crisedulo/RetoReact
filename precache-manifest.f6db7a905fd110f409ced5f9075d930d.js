6828476ed0abe36e35937fc8a7c9a3c148a41741
crisedulo <crisedulo@gmail.com>
2019-07-27 17:44:23 -0600

    Updates

---
+++ precache-manifest.f6db7a905fd110f409ced5f9075d930d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b1d56285bcb0b6c7a372b4d941b3f55",
+    "url": "/React/index.html"
+  },
+  {
+    "revision": "abbfca4a19d4f65c8f2d",
+    "url": "/React/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "2fc335b9620a60937940",
+    "url": "/React/static/css/main.fa93487a.chunk.css"
+  },
+  {
+    "revision": "abbfca4a19d4f65c8f2d",
+    "url": "/React/static/js/2.fe83be35.chunk.js"
+  },
+  {
+    "revision": "2fc335b9620a60937940",
+    "url": "/React/static/js/main.0e4f5892.chunk.js"
+  },
+  {
+    "revision": "283b01c4b2c734b504b3",
+    "url": "/React/static/js/runtime~main.d53f88d2.js"
+  }
+]);
